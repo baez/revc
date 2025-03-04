2e15da4ef4a0c9201a4aa69dbbfc1901e76d0eeb
gbk <george.bekari@gmail.com.com>
2025-03-04 02:31:30 -0800

    Malloc to Realloc.

--- src/dynmem.c
+++ src/dynmem.c
@@ -3,19 +3,38 @@
 
 int testcallocinteger(int countOfIntegersNeeded)
 {
-    int *arr = calloc(countOfIntegersNeeded, sizeof(int));  // Allocates memory for 10 integers and initializes all to 0.
+
+    int *arr = calloc(countOfIntegersNeeded, sizeof(int));
     if (arr == NULL) {
         perror("Memory allocation failed.");
         return EXIT_FAILURE;
     }
     
-    // arr now points to an array of 10 ints, all set to zero.
     for (int i = 0; i < countOfIntegersNeeded; i++) {
-        printf("before assignment [%d]: %d -- ", i, arr[i]);
+        printf("init [%d]: %d -- ", i, arr[i]);
         
         arr[i] = i;
-        printf("after: %d \n", arr[i]);
+        printf("set: %d \n", arr[i]);
     }
     
+    arr = realloc(arr, 2 * countOfIntegersNeeded * sizeof(int));  // Resize the array 
+    if (arr == NULL) {
+        perror("Memory reallocation failed.");
+        return EXIT_FAILURE;
+    }
+
+    // Note: Just as is the case with malloc, realloc doesn't perform any initialization. 
+    // Any memory past the memory that was present in the original block is left uninitialized.
+    // This means that the values in the new memory are indeterminate.
+    printf("--- after realloc to twice the size ---\n");
+    for (int i = 0; i < 2 * countOfIntegersNeeded; i++) {
+        printf("init [%d]: %d -- ", i, arr[i]);
+        
+        arr[i] = i;
+        printf("set: %d \n", arr[i]);
+    }
+
+    // Free the allocated memory
+    // Note: free(NULL) is guaranteed to be safe. From section 7.20.3.2/2 of the C99 standard -- no need to check for null here
     free(arr);
 }
